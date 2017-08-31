67dcb761678865de429d656ebc9f577aff267737
Kirill Krolik <yoursuffer@gmail.com>
2017-08-31 22:02:51 +0300

    play with dev server

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ function component() {
 
     // Lodash, now imported by this script
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.style.color = 'green';
 
     btn.innerText = 'Click me and check the console';
     btn.onclick = printMe;
--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,17 @@ module.exports = {
         }),
         new CleanWebpackPlugin(['./dist/'])
     ],
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: path.join(__dirname, '/dist'),
+        https: true,
+        port: 8085,
+        open: false,
+        overlay: {
+            warnings: true,
+            errors: true
+        }
+    },
     output: {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist')
